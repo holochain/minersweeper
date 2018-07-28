ed3c265d8ad50863f160c1cf96ac135cd8460a53
Willem Olding <willemolding@gmail.com>
2018-07-28 17:40:28 -0600

    adds simple test for newGame and getCurrentGame. passes

--- dna/minesweeper/minesweeper.js
+++ dna/minesweeper/minesweeper.js
@@ -6,37 +6,39 @@
 
 function newGame(payload) {
   var text = payload.text;
-  var size = payload.size;
-  var nMines = payload.nMines;
+  var size = payload.gameParams.size;
+  var nMines = payload.gameParams.nMines;
 
   var gameBoard = genGameBoard(text, size, nMines);
-  bundleStart(1);
+  // bundleStart(1, "");
   var gameHash = commit('gameBoard', gameBoard);
   commit('actionLinks', { Links: [ 
     { Base: makeHash('anchor', 'currentGames'), Link: gameHash, Tag: "" } ] 
   });  
-  bundleClose(true);
+  // bundleClose(true);
+  return gameHash;
 }
 
 function makeMove(payload) {
   var gameHash = payload.gameHash;
   var action = payload.move;
 
-  bundleStart(1);
+  // bundleStart(1, "");
   var actionHash = commit('action', action);
   commit('actionLinks', { Links: [ 
     { Base: gameHash, Link: actionHash, Tag: "" } ] 
   });
-  bundleClose(true);
+  // bundleClose(true);
 }
 
 function getCurrentGames() {
-  return getLinks(makeHash('anchor', 'currentGames'), {Load: true});
+  debug(makeHash('anchor', 'currentGames'));
+  return getLinks(makeHash('anchor', 'currentGames'), "", {Load: true});
 }
 
 function getState(payload) {
   var gameHash = payload.gameHash;
-  return getLinks(gameHash, {Load: true});
+  return getLinks(gameHash, "", {Load: true});
 }
 
 /*=====  End of Public Zome Functions  ======*/
@@ -54,22 +56,18 @@ function randInt(min, max) {
 
 function genGameBoard(text, size, nMines) {
   var mines = [];
-  
+
   for(var i = 0; i < nMines; i++) {
-    do {
-      var x = randInt(0, size.x);
-      var y = randInt(0, size.y);  
-    } while (!mines.some(function(elem) {
-      return x === elem.x && x === elem.y;
-    }))
-    mines.push({x:x, y:y})
+    var x = randInt(0, size.x);
+    var y = randInt(0, size.y);  
+    mines.push({x:x, y:y});
   }
 
   return {
     mines: mines,
     size: size, 
     text: text
-  }
+  };
 }
 
 
@@ -82,51 +80,48 @@ function genGameBoard(text, size, nMines) {
 ==========================================*/
 
 function genesis () {
-  commit('anchor', 'currentGames'); // ensure this always exists
+  var h = commit('anchor', 'currentGames'); // ensure this always exists
+  debug(h)
   return true;
 }
 
 
-function validateCommit (entryType, entry, header, pkg, sources) {
+function validatePut(entry_type, entry, header, pkg, sources) {
+  return validateCommit(entry_type, entry, header, pkg, sources);
+}
+function validateCommit(entry_type, entry, header, pkg, sources) {
   return true;
 }
 
-
-function validatePut (entryType, entry, header, pkg, sources) {
+function validateLink(linkingEntryType, baseHash, linkHash, pkg, sources) {
   return true;
+}
+function validateMod(entry_type, hash, newHash, pkg, sources) {
+  return true;
+}
+function validateDel(entry_type, hash, pkg, sources) {
+  return true;
+}
+function validatePutPkg(entry_type) {
+  return null;
+}
+function validateModPkg(entry_type) {
+  return null;
+}
+function validateDelPkg(entry_type) {
+  return null;
+}
+function validateLinkPkg(entry_type) {
+  return null;
 }
 
 function validateLink(entryType, hash, links, pkg, sources) {
   return true;
 }
 
-
-function validateMod (entryType, entry, header, replaces, pkg, sources) {
-  return true;
-}
-
-
-function validateDel (entryType, hash, pkg, sources) {
-  return true;
-}
-
-
-function validatePutPkg (entryType) {
+function validateDelPkg(entryType) {
   return null;
 }
 
-
-function validateModPkg (entryType) {
-  return null;
-}
-
-
-function validateDelPkg (entryType) {
-  return null;
-}
-
-function validateLinkPkg(entryType) {
-  return null;
-}
 
 /*=====  End of Required Callbacks  ======*/
