a0732aed9130f429365e54a9d5dc781e859f905e
Michael Dougherty <maackle.d@gmail.com>
2018-07-31 12:10:33 -0600

    Fix CellMatrix linter problems

--- ui-src/src/__tests__/CellMatrix.test.ts
+++ ui-src/src/__tests__/CellMatrix.test.ts
@@ -1,21 +1,21 @@
-import { CellMatrix } from "../CellMatrix";
+import CellMatrix from "../CellMatrix";
 
 const testGameBoard = {
+  creatorHash: "xxx",
+  description: "a game board for testing",
   mines: [{x: 10, y: 20}, {x: 20, y: 10}],
   size: {x: 100, y: 100},
-  description: "a game board for testing",
-  creatorHash: "xxx"
 }
 
 const smallTestBoard = {
+  creatorHash: "xxx",
+  description: "a small game board for testing",
   mines: [{x: 2, y: 2}],
   size: {x: 5, y: 5},
-  description: "a small game board for testing",
-  creatorHash: "xxx"
 }
 
 it('Can create a cell matrix instance with mines correctly set', () => {
-  let cm = new CellMatrix(testGameBoard);
+  const cm = new CellMatrix(testGameBoard);
   expect(cm.size).toEqual(testGameBoard.size);
   expect(cm.isMine({x: 3, y: 8})).toEqual(false);
   expect(cm.isMine({x: 10, y: 20})).toEqual(true);
@@ -23,7 +23,7 @@ it('Can create a cell matrix instance with mines correctly set', () => {
 });
 
 it('Can set the count of adjacent mines for each cell', () => {
-  let cm = new CellMatrix(testGameBoard);
+  const cm = new CellMatrix(testGameBoard);
   expect(cm.isMine({x: 10, y: 20})).toEqual(true);
   expect(cm.getAdjacentMines({x: 12, y: 20})).toEqual(0);
   expect(cm.getAdjacentMines({x: 11, y: 20})).toEqual(1);
@@ -31,7 +31,7 @@ it('Can set the count of adjacent mines for each cell', () => {
 });
 
 it('Can flag a cell and retrieve the value', () => {
-  let cm = new CellMatrix(testGameBoard);
+  const cm = new CellMatrix(testGameBoard);
   cm.flagCell({x: 1, y: 1}, "MYHASH!");
   expect(cm.isFlagged({x: 1, y: 1})).toEqual(true);
   expect(cm.isFlagged({x: 2, y: 2})).toEqual(false);
@@ -39,9 +39,9 @@ it('Can flag a cell and retrieve the value', () => {
   expect(cm.getFlag({x: 1, y: 1})).toEqual("MYHASH!");
   expect(cm.getFlag({x: 2, y: 2})).toEqual(null);
 });
- 
+
 it('Can reveal cells on a small grid', () => {
-  let cm = new CellMatrix(smallTestBoard);
+  const cm = new CellMatrix(smallTestBoard);
   expect(cm.triggerReveal({x: 0, y: 0})).toEqual(25 - 1);
   expect(cm.isRevealed({x: 0, y: 0})).toEqual(true)
   expect(cm.isRevealed({x: 2, y: 2})).toEqual(false)
@@ -49,7 +49,7 @@ it('Can reveal cells on a small grid', () => {
 });
 
 it('Can reveal cells on a large grid', () => {
-  let cm = new CellMatrix(testGameBoard);
+  const cm = new CellMatrix(testGameBoard);
   expect(cm.triggerReveal({x: 0, y: 0})).toEqual(100*100 - 2);
   expect(cm.isRevealed({x: 0, y: 0})).toEqual(true)
   expect(cm.isRevealed({x: 10, y: 20})).toEqual(false)
