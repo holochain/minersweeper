ecaeb8d7409e41b1099573fec68c40e2afef7e1b
Lisa Jetton <ldjetton21@gmail.com>
2018-07-31 11:14:26 -0600

    mountain animation

--- ui-src/src/components/Lobby.tsx
+++ ui-src/src/components/Lobby.tsx
@@ -9,12 +9,17 @@ import {fetchJSON} from '../common';
 import {GameParams} from '../types';
 
 import CreateGameForm from './CreateGameForm'
+// import Bitcoin from "./bitcoin.png"
+// import { Bitcoin, DarkBitcoin, Moreno }
+
 
 interface ILobbyProps {
   games: List<GameParams>
 }
 
 class Lobby extends React.Component<any, {}> {
+  public CryptoIcons = [];
+  // CryptoIcons.push(Bitcoin, ... )
 
   public componentWillMount() {
     setInterval(
@@ -29,12 +34,35 @@ class Lobby extends React.Component<any, {}> {
      )
   }
 
+  // public renderCryptoIcons() {
+  //   return CryptoIcons.map((icon) => {
+  //     return (
+  //       <img
+  //         key={icon}
+  //         src={icon}/>
+  //     );
+  //   });
+  // }
+
   public render() {
     const allGames = this.props.allGames
     return (
-      <div className="Lobby">
-        <CreateGameForm/>
-        <GameList allGames={allGames}/>
+      <div>
+        <div className="screen">
+          <div className="Lobby">
+            <div className="lobby-jumbotron">
+              <h1 className="lobby-header">Welcome to Minersweeper</h1>
+            </div>
+            <div className="lobby-register">
+              <h4>Create a Game Below</h4>
+              <CreateGameForm/>
+              <GameList allGames={allGames}/>
+            </div>
+          </div>
+          <p>
+          <img/>
+           {/* {this.renderCryptoIcons()} */}
+        </div>
       </div>
     );
   }
@@ -48,6 +76,7 @@ const GameList = ({allGames}) => {
         return <li key={hash}>
           <Link to={`/game/${hash}`}>
             {game.description}
+            {console.log("game", game)}
           </Link>
         </li>
       })
