f44384fc4f87d54435e5273a488e2aaa71b27408
Willem Olding <willemolding@gmail.com>
2018-07-31 16:27:51 -0600

    fixes test framework for new structure. all tests pass

--- ui-src/src/__tests__/scoring.test.ts
+++ ui-src/src/__tests__/scoring.test.ts
@@ -1,6 +1,5 @@
 import {getScore, isMine } from "../scoring";
-import {ActionType} from "../types";
-
+import { Action } from "../../../minesweeper";
 
 const testGameBoard = {
   mines: [{x: 10, y: 20}, {x: 20, y: 10}],
@@ -10,10 +9,10 @@ const testGameBoard = {
 }
 
 const testActions: Action[] = [
-  {position: {x: 0, y: 0}, actionType: ActionType.Reveal, agentHash: "XXX"},
-  {position: {x: 10, y: 10}, actionType: ActionType.Flag, agentHash: "XXX"},
-  {position: {x: 10, y: 20}, actionType: ActionType.Reveal, agentHash: "XXX"},
-  {position: {x: 20, y: 10}, actionType: ActionType.Flag, agentHash: "XXX"}
+  {position: {x: 0, y: 0}, actionType: "reveal", agentHash: "XXX"},
+  {position: {x: 10, y: 10}, actionType: "flag", agentHash: "XXX"},
+  {position: {x: 10, y: 20}, actionType: "reveal", agentHash: "XXX"},
+  {position: {x: 20, y: 10}, actionType: "flag", agentHash: "XXX"}
 ];
 
 it('Can identify a mine in the game board using isMine', () => {
@@ -28,9 +27,9 @@ it('Can reduce a sequence of actions to a score', () => {
 });
 
 it('Can ignore subsequent actions in the same tile irrespective of hash', () => {
-  let newTestActions = testActions.concat({position: {x: 20, y: 10}, actionType: ActionType.Flag, agentHash: "YYY"})
+  let newTestActions = testActions.concat({position: {x: 20, y: 10}, actionType: "flag", agentHash: "YYY"})
   expect(getScore(testGameBoard, newTestActions, "XXX")).toEqual(-6);
   expect(getScore(testGameBoard, newTestActions, "YYY")).toEqual(0);
-  newTestActions = newTestActions.concat({position: {x: 11, y: 11}, actionType: ActionType.Reveal, agentHash: "YYY"})
+  newTestActions = newTestActions.concat({position: {x: 11, y: 11}, actionType: "reveal", agentHash: "YYY"})
   expect(getScore(testGameBoard, newTestActions, "YYY")).toEqual(1);
 })
