a8a3d39a8d7f56cbc98a53368a968e9369e939e4
Willem Olding <willemolding@gmail.com>
2018-08-01 12:35:20 -0600

    getScore is now getScores that returns a map between hashes and scores for all agents that have made a move

--- ui-src/src/scoring.ts
+++ ui-src/src/scoring.ts
@@ -7,44 +7,51 @@ export const CLICK_NON_MINE = 1;
 export const FLAG_MINE = 5;
 export const FLAG_NON_MINE = -2;
 
-export const getScore = (gameBoard: GameBoard, actions: Action[], agentHash: Hash) => {
+// returns the scores for all players that have made moves in the game
+export const getScores = (gameBoard: GameBoard, actions: Action[]): Map<Hash, number> => {
+  type scoreMap = Map<Hash, number>
 
-  const scoreReducer = (acc: number, action: Action): number => {
-    switch(action.actionType) {
-      case "flag":
-        if(isMine(gameBoard, action.position)) {
-          return acc + FLAG_MINE;
-        } else {
-          return acc + FLAG_NON_MINE;
-        }
-      case "reveal":
-        if(isMine(gameBoard, action.position)) {
-          return acc + CLICK_MINE;
-        } else {
-          return acc + CLICK_NON_MINE;
-        }
+  return actions.reduce<scoreMap>((scores: scoreMap, action: Action, index: number): scoreMap => {
+
+    let newScore = scores.get(action.agentHash);
+    if (newScore === undefined) { newScore = 0; }
+
+    if (isFirst(action, index, actions)) {
+      switch(action.actionType) {
+        case "flag":
+          if(isMine(gameBoard, action.position)) {
+            newScore += FLAG_MINE;
+          } else {
+            newScore += FLAG_NON_MINE;
+          }
+          break;
+        case "reveal":
+          if(isMine(gameBoard, action.position)) {
+            newScore += CLICK_MINE;
+          } else {
+            newScore += CLICK_NON_MINE;
+          }
+          break;
+        default:
+          break;
+      }
     }
-    return acc;
-  };
 
-  // only consider score producing actions (flag or reveal)
-  // filter actions so only the first action in each position is counted (assumes already sorted)
-  // then count only actions from this agentHash
-  // then reduce to a single score value
-  return actions
-    .filter((action: Action, index, arr): boolean => {
-      if (action.actionType === "chat") { return false; }
-      return arr.findIndex((compareAction: Action): boolean => {
-        if (compareAction.actionType === "chat") { return false; }
-        return action.position.x === compareAction.position.x && action.position.y === compareAction.position.y;
-      }) === index; // returns true for the first action at each position
-    })
-    .filter(action => {
-      return action.agentHash === agentHash;
-    })
-    .reduce(scoreReducer, 0);
+    return scores.set(action.agentHash, newScore);
+  }, new Map<Hash, number>());
 
-};
+}
+
+// only the first action on each square is allowed to generate a score.
+// Assumes actions are sorted ascending in time
+const isFirst = (action: Action, index: number, actions: Action[]): boolean => {
+  if (action.actionType === "chat") { return false; }
+  return actions.findIndex((compareAction: Action): boolean => {
+    if (compareAction.actionType === "chat") { return false; }
+    return action.position.x === compareAction.position.x && action.position.y === compareAction.position.y;
+  }) === index; // returns true for the first action at each position
+}
+
 
 export const isMine = (gameBoard: GameBoard, pos: Pos): boolean => {
   return gameBoard.mines.some(mine => {
