91a1d67f21f51db315b8d09260bbab567ed7242d
Michael Dougherty <maackle.d@gmail.com>
2018-08-02 15:34:39 -0600

    Somewhat jumpy d-pad panning

--- ui-src/src/components/Field.tsx
+++ ui-src/src/components/Field.tsx
@@ -10,7 +10,14 @@ import {Hash} from '../../../holochain';
 import Cell from './Cell';
 import CellMatrix from '../CellMatrix';
 
-import {CELL_SIZE, MARGIN_CELLS} from '../common';
+import {
+  CELL_SIZE,
+  FETCH_ACTIONS_INTERVAL,
+  MARGIN_CELLS,
+  fetchCurrentGames,
+  fetchJSON
+} from '../common';
+import store from '../store';
 
 
 // from https://github.com/bvaughn/react-virtualized/blob/master/docs/Grid.md#overscanindicesgetter
@@ -34,9 +41,70 @@ type FieldProps = {
   myActions: number  // NB: dumb hack to ensure updates
 }
 
-class Field extends React.Component<FieldProps, {}> {
+class Field extends React.Component<FieldProps | any, {}> {
+
+  private grid = React.createRef()
+  private panKeys: any = {}
+  private actionsInterval: any = null
+
+  constructor(props) {
+    super(props)
+    // this.state {
+    //   panKeys: false
+    // }
+  }
+
+  public shouldComponentUpdate(nextProps, nextState) {
+    return !this.panKeys
+  }
+
+  public componentWillUnmount() {
+    clearInterval(this.actionsInterval)
+  }
+
+  public componentWillMount() {
+    this.startPolling()
+    const offsets = {
+      37: {x: -1, y: 0},
+      38: {x: 0, y: -1},
+      39: {x: 1, y: 0},
+      40: {x: 0, y: 1},
+    }
+    window.addEventListener('keydown', e => {
+      const code = e.keyCode
+      if (this.panKeys[code]) {
+        return
+      }
+      this.stopPolling()
+      const offset = offsets[code]
+      const speed = CELL_SIZE
+      this.panKeys[code] = setInterval(
+        () => {
+          const grid : any = this.grid.current!
+          const container = grid._scrollingContainer
+          if (offset) {
+            // container.scrollLeft += offset.x * speed
+            // container.scrollTop += offset.y * speed
+            const x = container.scrollLeft + offset.x * speed
+            const y = container.scrollTop + offset.y * speed
+            grid.scrollToPosition({scrollLeft: x, scrollTop: y})
+          }
+
+        }, 10
+      )
+    })
+    window.addEventListener('keyup', e => {
+      const code = e.keyCode
+      clearInterval(this.panKeys[code])
+      this.panKeys[code] = null
+      if (Object.keys(this.panKeys).some(k => this.panKeys[k])) {
+        this.startPolling()
+      }
+    })
+  }
 
   public render() {
+    console.log('render')
     const columns = this.props.matrix.size.x
     const rows = this.props.matrix.size.y
     const cellSize = CELL_SIZE
@@ -45,6 +113,7 @@ class Field extends React.Component<FieldProps, {}> {
       <div className="field-container">
         <AutoSizer>{
           ({width, height}) => <Grid
+            ref={ this.grid }
             cellRenderer={this.CellWrapped}
             columnCount={columns + MARGIN_CELLS * 2}
             rowCount={rows + MARGIN_CELLS * 2}
@@ -52,7 +121,7 @@ class Field extends React.Component<FieldProps, {}> {
             rowHeight={cellSize}
             height={height}
             isScrollingOptOut={true}
-            tabIndex=""
+            tabIndex={null}
             width={width}
             overscanColumnCount={20}
             overscanRowCount={20}
@@ -73,6 +142,30 @@ class Field extends React.Component<FieldProps, {}> {
       rowIndex={rowIndex - MARGIN_CELLS}
       {...props}/>
    )
+
+
+  private startPolling() {
+    const hash = this.props.gameHash
+    if (hash) {
+      const fetchActions = () => fetchJSON('/fn/minersweeper/getState', {
+        gameHash: hash
+      }).then(actions => {
+        store.dispatch({
+          type: 'FETCH_ACTIONS',
+          actions
+        })
+      })
+
+      fetchActions()
+      this.actionsInterval =  setInterval(
+        fetchActions, FETCH_ACTIONS_INTERVAL
+      )
+    }
+  }
+
+  private stopPolling() {
+    clearInterval(this.actionsInterval)
+  }
 }
 
 const mapStateToProps = state => ({
--- ui-src/src/components/GameView.tsx
+++ ui-src/src/components/GameView.tsx
@@ -45,34 +45,18 @@ class GameView extends React.Component<any, { loading: boolean }> {
       } else {
         dispatchViewGame()
       }
-
-      const fetchActions = () => fetchJSON('/fn/minersweeper/getState', {
-        gameHash: hash
-      }).then(actions => store.dispatch({
-        type: 'FETCH_ACTIONS',
-        actions
-      }))
-      fetchActions()
-      this.actionsInterval = setInterval(
-        fetchActions, FETCH_ACTIONS_INTERVAL
-      )
     }
-  }
-
-  public componentWillUnmount() {
-    clearInterval(this.actionsInterval)
   }
 
   public render() {
     const { currentGame } = store.getState()
-    console.log("currentGame State:", { currentGame })
 
     if (currentGame) {
       if (currentGame.gameOver === true) {
         const { matrix, gameHash } = currentGame
         return <div className="game-container">
-          <Field gameHash={gameHash} matrix={matrix} actions={[]} />
-          <GameOver />
+          <Field gameHash={gameHash} matrix={matrix} />
+          <GameHUD />
         </div>
       }
       else {
@@ -92,7 +76,5 @@ class GameView extends React.Component<any, { loading: boolean }> {
 const mapStateToProps = state => ({
   currentGame: state.currentGame
 })
-
-// export default GameView;
 
 export default connect(mapStateToProps)(GameView);
