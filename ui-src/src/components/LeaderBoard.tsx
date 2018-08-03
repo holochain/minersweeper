89696202c17211ea3c8bf9b1e57e15e6b8d23421
Yang Hu <yh2948@columbia.edu>
2018-08-03 15:57:34 -0600

    delete fetchText from common and leaderboard

--- ui-src/src/components/LeaderBoard.tsx
+++ ui-src/src/components/LeaderBoard.tsx
@@ -2,7 +2,6 @@ import * as React from 'react';
 import {connect} from 'react-redux';
 import {Hash} from "../../../holochain";
 import {Map, fromJS} from 'immutable';
-import {fetchText} from '../common'
 
 import store from '../store';
 import Jdenticon from './Jdenticon';
