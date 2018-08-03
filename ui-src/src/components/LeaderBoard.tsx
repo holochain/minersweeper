60130d0c8986f9bb34781b05d24baa7556a76065
Yang Hu <yh2948@columbia.edu>
2018-08-02 23:32:13 -0600

    Add user handler and sort

--- ui-src/src/components/LeaderBoard.tsx
+++ ui-src/src/components/LeaderBoard.tsx
@@ -1,27 +1,43 @@
 import * as React from 'react';
 import {connect} from 'react-redux';
 import {Hash} from "../../../holochain";
-import {Map} from 'immutable';
+import {Map, fromJS} from 'immutable';
+import {fetchIdentities} from '../common'
 
 import store from '../store';
 import Jdenticon from './Jdenticon';
 
 type LeaderBoardProps = {
   scores: Map<Hash, number>,
+  allPlayerHandles: Map<Hash, string>,
 }
 
 class LeaderBoard extends React.Component<LeaderBoardProps, {}> {
 
+
   public render () {
     // Fetch the score
-    const {scores} = this.props
+    const {scores, allPlayerHandles} = this.props
 
+    const tempUserScoreMap:any[] = []
+    // Fetch the identity
+    if(scores !== null) {
+      scores.forEach((agentScore:number, agentHash:Hash) => {
+        const agentHandler:string = allPlayerHandles.get(agentHash!)
+        tempUserScoreMap.push([agentHandler, agentScore])
+      })
+    }
 
+    const sortedUserScoreMap = fromJS(tempUserScoreMap).sort((a, b) => {
+      if (a < b) { return -1; }
+      if (a > b) { return 1; }
+      if (a === b) { return 0; }
+    });
 
     // Change the user hash into user name
     return (
     <div>
-      {scores}
+      {sortedUserScoreMap}
     </div>
     )
   }
@@ -29,6 +45,7 @@ class LeaderBoard extends React.Component<LeaderBoardProps, {}> {
 
 const mapStateToProps = state => ({
   scores: state.currentGame.scores,
+  allPlayerHandles: state.identities,
 })
 
 export default connect(mapStateToProps)(LeaderBoard);
