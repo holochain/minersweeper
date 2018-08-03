68337c99b5b01f36b11cc1c23438798b93287337
Michael Dougherty <maackle.d@gmail.com>
2018-08-02 22:59:48 -0600

    More robust & consistent UI for various cell states

--- ui-src/src/common.ts
+++ ui-src/src/common.ts
@@ -10,13 +10,15 @@ export const MARGIN_CELLS = 2
 
 // when mouse moves within this many pixels of the edge, pan the field
 export const MOUSE_PAN_MARGIN = 75
-export const MOUSE_PAN_SPEED = CELL_SIZE
-export const KEY_PAN_SPEED = CELL_SIZE
+
+// pixels to move per panning update
+export const MOUSE_PAN_SPEED = CELL_SIZE * 1.0
+export const KEY_PAN_SPEED = CELL_SIZE * 1.0
 
 // millisecond intervals for various polling tasks
 export const FETCH_ACTIONS_INTERVAL = 1500
 export const FETCH_LOBBY_INTERVAL = 3000
-export const PAN_INTERVAL = 10
+export const PAN_INTERVAL = 50
 
 export const xor = (a, b) => a && !b || !a && b
 
