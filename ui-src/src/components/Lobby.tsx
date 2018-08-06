1ca29e8a92a7a86ec8d8195b1e09d730c2a458a8
Michael Dougherty <maackle.d@gmail.com>
2018-08-06 11:08:30 -0600

    Use identities in lobby game list

--- ui-src/src/components/Lobby.tsx
+++ ui-src/src/components/Lobby.tsx
@@ -13,9 +13,6 @@ import { fetchJSON, FETCH_LOBBY_INTERVAL } from '../common';
 
 import CreateGameForm from './CreateGameForm'
 
-// interface LobbyProps {
-//   games: List<GameParams>
-// }
 
 class Lobby extends React.Component<any, any> {
   public cryptoIcons = ["btc", "eth", "xmr", "ltc", "doge", "drgn", "bcc", "kmd", "dbc", "elix", "mkr", "powr", "xvg", "zec", "huc", "tel", "pot", "pay", "ox", "nxs", "nmc", "lrc"];
@@ -64,7 +61,7 @@ class Lobby extends React.Component<any, any> {
   }
 
   public render() {
-    const allGames = this.props.allGames
+    const {allGames, identities} = this.props
     const modalDiv = (
       <div className="interstitial-modal-overlay">
         <div className="interstitial-modal">
@@ -99,7 +96,7 @@ class Lobby extends React.Component<any, any> {
               </div>
             </button>
           </div>
-            <GameList allGames={allGames}/>
+          <GameList allGames={allGames} identities={identities}/>
         </div>
         { this.state.showModal ? modalDiv : null }
       </div>
@@ -107,7 +104,7 @@ class Lobby extends React.Component<any, any> {
   }
 }
 
-const GameList = ({ allGames }) => {
+const GameList = ({ allGames, identities }) => {
   // console.log("allGames", allGames );
   // console.log("allGames.size", allGames.size );
   if(allGames && allGames.size < 1) {
@@ -132,25 +129,30 @@ const GameList = ({ allGames }) => {
         </thead>
         <tbody>
           {
-            Object.keys(allGames.toJS()).map(hash => {
+            allGames.keySeq().map(hash => {
               const game = allGames.get(hash)
+              const {creatorHash, description, mines, size} = game
+              const author = (
+                identities.get(creatorHash)
+                || creatorHash.substring(0,5) + '...'
+              )
               return <tr key={hash}>
                 <td className="game-description">
-                  {game.description}
+                  {description}
                 </td>
                 <td>
-                  <Jdenticon style={{marginRight: 2}} className="middle-align-item" size={30} hash={game.creatorHash}/>
-                  <span className="middle-align-item">{game.creatorHash.substring(0,5)}</span>
+                  <Jdenticon style={{marginRight: 2}} className="middle-align-item" size={30} hash={creatorHash}/>
+                  <span className="middle-align-item">{author}</span>
                 </td>
-                <td>{game.mines.length}</td>
-                <td>{game.size.x} x {game.size.y}</td>
+                <td>{mines.length}</td>
+                <td>{size.x} x {size.y}</td>
                 <td>
                   <Link to={`/game/${hash}`}>
                     <button className="play-button">Play</button>
                   </Link>
                 </td>
               </tr>
-            })
+            }).toJS()
           }
         </tbody>
       </table>
@@ -161,8 +163,8 @@ const GameList = ({ allGames }) => {
 }
 
 
-const mapStateToProps = ({ allGames }) => ({
-  allGames
+const mapStateToProps = ({ allGames, identities }) => ({
+  allGames, identities
 })
 
 export default connect(mapStateToProps)(Lobby);
