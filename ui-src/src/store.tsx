1ae5c12823f2f3592b8556ee678ca2fefd796d88
Michael Dougherty <maackle.d@gmail.com>
2018-08-07 12:45:49 -0600

    Simplify comparator

--- ui-src/src/store.tsx
+++ ui-src/src/store.tsx
@@ -47,13 +47,7 @@ function reduceGame (state: StoreState, action: ReduxAction): StoreGameState {
       break;
     }
     case 'FETCH_ACTIONS': {
-      action.actions.sort((a, b) =>
-        a.timestamp < b.timestamp
-        ? -1
-        : a.timestamp > b.timestamp
-        ? 1
-        : 0
-      )
+      action.actions.sort((a, b) => a.timestamp - b.timestamp)
       action.actions.forEach(a => {
         matrix.takeAction(a)
       })
